1dbcb9dba4220f5208d5897f2cdb28ccab6bfffa
aori___ <hilariomarkryan3@gmail.com>
2025-04-29 21:56:19 +0800

    Add basic head content including halfmoon, awesomefont, and custom main css file

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
 <?php
-    include("database.php");
-    include("index_php.php");
+    // include("database.php");
+    // include("index_php.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>MusicAh</title>
-</head>
+
+<?php include 'assets/components/head.component.php'; ?>
+
 <body>
     <h1>MusicAh</h1>
-    
 
     <h2>Add Song</h2>
     <p><?php echo $add_err?></p>
