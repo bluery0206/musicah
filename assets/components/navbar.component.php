adef3b5bef4e6aa22e65c3a14e20ce20eb9ca469
aori___ <hilariomarkryan3@gmail.com>
2025-04-29 23:09:19 +0800

    Add basic nav bar

---
+++ assets/components/navbar.component.php
@@ -0,0 +1,33 @@
+
+<nav class="navbar navbar-expand-md"
+    style="background-color: var(--bs-content-bg); border-bottom: var(--bs-border-width) solid var(--bs-content-border-color);">
+    <div class="container">
+        <div class="row d-flex justify-content-between w-100">
+            <div class="col-auto">
+                <a class="navbar-brand" href="http://localhost/php-projects/musicah/">
+                    <i class="fa-solid fa-record-vinyl d-inline-block align-text-center"></i>
+                    <span>Musicah</span>
+                </a>
+            </div>
+            <div class="col-auto">
+                <div class="row g-1">
+                    <div class="col-auto">
+                        <a href="https://github.com/bluery0206/musicah" class="btn px-1">
+                            <i class="fa-brands fa-github"></i>
+                        </a>
+                    </div>
+                    <div class="col-auto">
+                        <a href="https://web.facebook.com/hilario.markryan.i" class="btn px-1">
+                            <i class="fa-brands fa-facebook"></i>
+                        </a>
+                    </div>
+                    <div class="col-auto">
+                        <a href="https://bsky.app/profile/aorikasumi.bsky.social" class="btn px-1">
+                            <i class="fa-brands fa-bluesky"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
