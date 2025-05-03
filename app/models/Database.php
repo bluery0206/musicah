956f0409bc8a3c9b8a22e9823f344aa1b249dfe2
aori___ <hilariomarkryan3@gmail.com>
2025-05-03 20:48:37 +0800

    Update database connection from a function to a class so that we can extend our models in it improving security

---
+++ app/models/Database.php
@@ -0,0 +1,17 @@
+<?php 
+
+class Database {
+    protected function connect(): PDO {
+        $hname  = "localhost";
+        $uname  = "root";
+        $pwd    = "";
+        $dbname = "test-db";
+        
+        $dsn = "mysql:host=$hname;dbname=$dbname";
+        
+        $conn = new PDO($dsn, $uname, $pwd);
+        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        
+        return $conn;
+    }
+}
