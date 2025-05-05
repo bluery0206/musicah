2439595b5ee1340bb1c87bcba82c9de2bf3a112f
aori___ <hilariomarkryan3@gmail.com>
2025-05-05 17:39:45 +0800

    Add `getUserByID` and `getUserByUsername`

--- app/models/User.php
+++ app/models/User.php
@@ -1,35 +1,70 @@
 <?php
 
 class User extends Database {
-    private int $id;
-
-    public function __construct($id) {
-        $this->id = $id;
-    }
-
-    protected function getUsername() {
+    protected function getUsername(int $id) {
         $query = "SELECT username FROM users WHERE id = ?";
         $stmt = $this->connect()->prepare($query);
-        $stmt->execute([$this->id]);
+        $stmt->execute([$id]);
         $username = $stmt->fetch();
 
-        if (!$stmt) {
+        if (!$username) {
             throw new Exception("User not found.");
         }
 
         return $username;
     }
 
-    protected function getPassword(): string {
+    protected function getPassword(int $id): string {
         $query = "SELECT password FROM users WHERE id = ?";
         $stmt = $this->connect()->prepare($query);
-        $stmt->execute([$this->id]);
-        $user = $stmt->fetch();
+        $stmt->execute([$id]);
+        $password = $stmt->fetch();
 
-        if (!$stmt) {
+        if (!$password) {
             throw new Exception("User not found.");
         }
 
-        return $user->username;
+        return $password;
+    }
+
+    protected function getUserByID(int $id) {
+        $query = "SELECT * FROM users WHERE id = ?";
+        $stmt = $this->connect()->prepare($query);
+        $stmt->execute([$id]);
+        $user = $stmt->fetch();
+
+        if (!$user) {
+            throw new Exception("User not found.");
+        }
+
+        return $user;
+    }
+
+    protected function getUserByUsername(string $username) {
+        $query = "SELECT * FROM users WHERE username = ?";
+        $stmt = $this->connect()->prepare($query);
+        $stmt->execute([$username]);
+        $user = $stmt->fetch();
+
+        if (!$user) {
+            throw new Exception("User not found.");
+        }
+
+        return $user;
+    }
+
+    protected function userIDExists(int $id):bool {
+        $query = "SELECT id FROM users WHERE id = ?";
+        $stmt = $this->connect()->prepare($query);
+        $stmt->execute([$id]);
+
+        return $stmt->fetch()->id ? true : false;
+    }
+
+    protected function usernameExists(string $username) {
+        $query = "SELECT username FROM users WHERE username = ?";
+        $stmt = $this->connect()->prepare($query);
+        $stmt->execute([$username]);
+        return $stmt->fetch()->username ? true : false;
     }
 }
