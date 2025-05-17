ecf54e86ca3dda7a225c4f99db5d4e22f9f6136e
aori___ <hilariomarkryan3@gmail.com>
2025-05-17 22:12:28 +0800

    Refactor globals.php to improve URL handling and session management; implement user redirection for authentication.

--- config/globals.php
+++ config/globals.php
@@ -1,8 +1,38 @@
 <?php
 
-$_SERVER['PROJECT_DIR_NAME'] = "musicah";
-$_SERVER['PARENT_DIR_NAME'] = "php-projects";
-$_SERVER['HTTP_PROTOCOL'] = strtolower(explode("/", $_SERVER['SERVER_PROTOCOL'])[0]);
-$_SERVER['URL_INDEX'] = $_SERVER['HTTP_PROTOCOL'] . "://" . $_SERVER['SERVER_NAME'] . "/" . $_SERVER['PARENT_DIR_NAME'] . "/". $_SERVER['PROJECT_DIR_NAME'] . "/";
-$_SERVER['URL_PUBLIC'] = $_SERVER['INDEX_URL'] . "public/";
-$_SERVER["IN_PUBLIC_DIR"] = strrpos($_SERVER["PHP_SELF"],"public");
+session_start();
+
+$serverProtocol = strtolower(explode("/", $_SERVER['SERVER_PROTOCOL'])[0]);
+$baseUrl = $serverProtocol . "://" . $_SERVER['HTTP_HOST'] . "/";
+
+// Populate if the project is in a subdirectory of htdocs
+// Example:
+//      $dir = "parentDirName/projectDirName/";
+$dir = "php-projects/musicah/";
+
+if (isset($dir)) {
+    $baseUrl = "{$baseUrl}{$dir}";
+}
+
+$publicDir = "public/";
+$publicUrl = "{$baseUrl}{$publicDir}";
+
+
+// URLs
+
+$publicUrls = (object)[
+    "signin" => "{$publicUrl}signin.php",
+    "signup" => "{$publicUrl}signup.php",
+    "signout" => "{$publicUrl}signout.php",
+];
+
+// In attempt to follow how Django urls work
+$urls = (object)[
+    "index" => $baseUrl,
+    "public" => $publicUrls,
+];
+
+// Forces user to login before seeing the website
+if (!isset($_SESSION['user']) && !str_contains($_SERVER['PHP_SELF'], "signin")) {
+    header("Location:{$urls->public->signin}");
+}
