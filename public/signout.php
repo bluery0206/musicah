36de801e97654aef7a880b1ba9bc68cbd65efe2b
aori___ <hilariomarkryan3@gmail.com>
2025-05-23 13:34:48 +0800

    Refactored files structure and removed unused files

--- public/signout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once '../config/globals.php';
-    include_once '../autoload.php';
-    
-    $userController = new UserController();
-    $userController->signout();
-    header("Location:{$urls->index}");
